824cb3727f97d937ce47c9f547e3454b9a7fcb5d
greyhawk <wu.tongqing@gmail.com>
2015-11-30 17:27:39 +0800

    modify modules go

--- modules.go
+++ modules.go
@@ -1,4 +1,6 @@
+package main
+
 import (
-  _ "github.com/looplab/logspout-logstash"
-  _ "github.com/gliderlabs/logspout/transports/udp"
+	_ "github.com/looplab/logspout-logstash"
+	_ "github.com/gliderlabs/logspout/transports/udp"
 )
